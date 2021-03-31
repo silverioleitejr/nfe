7d365b0875f94e6c6eccf88a5170d975b869e571
og1tecnologia <48569732+og1tecnologia@users.noreply.github.com>
2021-03-31 11:19:53 -0300

    OK

--- code/TestSchemaXML.py
+++ code/TestSchemaXML.py
@@ -7,14 +7,14 @@ from lxml import etree
 class TestSchemaXML(unittest.TestCase):
     def setUp(self):
         pass
- 
+   
     def test_schema_pl_006s(self):
-        nfe = etree.parse("/home/lukaswilkeer/web2py/applications/spotnfe/uploads/nfe.xml")
-        xmlschema_doc = etree.parse("/home/lukaswilkeer/web2py/applications/spotnfe/modules/schemas/PL_006s/leiauteNFe_v2.00.xsd")
+        nfe = etree.parse("https://raw.githubusercontent.com/silverioleitejr/python-nfe/main/XML/NFe35170171322150001301550000000477521876550573_procNFe.xml")
+        xmlschema_doc = etree.parse("https://raw.githubusercontent.com/silverioleitejr/python-nfe/main/XSD/leiauteNFe_v4.00.xsd/leiauteNFe_v4.00.xsd")
         xmlschema = etree.XMLSchema(xmlschema_doc)
         validado  = xmlschema.validate(nfe)
         if not validado:# se  for invalido..
-            print xmlschema.error_log
+            print (xmlschema.error_log)
  
  
 if __name__ == '__main__':
